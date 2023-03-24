2c32b0fd2b568bafc927101d9e3dfb6c65f0d04a
Dylan Jacquet <d.jacquet@apparence.io>
2023-03-24 11:25:44 +0100

    add starting sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./assets/**/*.js", "./templates/**/*.html.twig"],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': colors.white,
+      'transparent-white': '#7C767F',
+      'transparent-hover': '#CECBD0',
+      'dark-purple': "#14081B",
+      'dark-medium-purple': "#1B0F20",
+      'medium-purple': "#2B1534",
+      'electric-purple': "#B53FF0",
+      'medium-light-purple': '#431857',
+      'font-blue': "#5959BE",
+      'profile-blue': "#7B7DFF",
+    }
   },
   plugins: [],
 };

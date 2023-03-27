5d94a8b2f3268c12b8b861d3c0e0f2b7d1703c75
BDanielo <daniel@armieux.fr>
2023-03-27 08:38:29 +0200

    MyGames, AllGames
    both now work [raw]

--- src/Controller/AllGamesController.php
+++ src/Controller/AllGamesController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AllGamesController extends AbstractController
 {
-    #[Route('/all/games', name: 'app_all_games')]
-    public function index(): Response
+    #[Route('/all-games', name: 'app_all_games')]
+    public function index(GameRepository $gameRepository): Response
     {
+        // get all games
+        $games = $gameRepository->findAll();
+
         return $this->render('all_games/index.html.twig', [
             'controller_name' => 'AllGamesController',
+            'games' => $games,
         ]);
     }
 }
--- src/Controller/MyGamesController.php
+++ src/Controller/MyGamesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class MyGamesController extends AbstractController
 {
     #[Route('/user/my-games', name: 'app_my_games')]
-    public function index(): Response
+    public function index(GameRepository $gameRepository): Response
     {
+        // get the current user
+        $user = $this->getUser();
+
+        // get all games owned by the current user
+        $games = $user->getGamesOwned();
+
         return $this->render('my_games/index.html.twig', [
             'controller_name' => 'MyGamesController',
+            'games' => $games,
         ]);
     }
 }

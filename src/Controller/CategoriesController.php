98244d4fd383fde921ac7075e96ab30fa9642c80
Dylan Jacquet <d.jacquet@apparence.io>
2023-03-27 17:19:56 +0200

    update all games, fix a bit categories controller

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\GamesRepository;
+use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoriesController extends AbstractController
 {
     #[Route('/categories/{id}', name: 'app_categories')]
-    public function index(int $id, GamesRepository $gamesRepository): Response
+    public function index(int $id, GameRepository $gamesRepository): Response
     {
 
         $games = $gamesRepository->findBy(['category' => $id]);

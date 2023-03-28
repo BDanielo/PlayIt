1a300b82945541fd8df37ee8f944e551b9220d85
BDanielo <daniel@armieux.fr>
2023-03-28 14:58:18 +0200

    Reviews
    Only review purchased games

--- src/Services/GameReviewService.php
+++ src/Services/GameReviewService.php
@@ -40,6 +40,22 @@ class GameReviewService
     {
         // get user reviews
         $userReviews = $user->getReviews();
+
+        // check if the user owns the game
+        $userGames = $user->getGamesOwned();
+
+        $owned = false;
+
+        foreach ($userGames as $userGame) {
+            if ($userGame == $game) {
+                $owned = true;
+            }
+        }
+
+        // if (!$owned) {
+        //     return true;
+        // }
+
         // for each review check if the game is the same
         foreach ($userReviews as $review) {
             if ($review->getGames() == $game) {

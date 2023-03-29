918109afe842931f977897ab6545d741a653278a
BDanielo <daniel@armieux.fr>
2023-03-29 11:09:47 +0200

    promotions

--- migrations/Version20230328130737.php
+++ migrations/Version20230328130737.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230328091821 extends AbstractMigration
+final class Version20230328130737 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20230328091821 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `order` ADD total DOUBLE PRECISION NOT NULL');
+        $this->addSql('ALTER TABLE game ADD promotion DOUBLE PRECISION DEFAULT NULL, ADD promotion_start DATETIME DEFAULT NULL, ADD promotion_end DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `order` DROP total');
+        $this->addSql('ALTER TABLE game DROP promotion, DROP promotion_start, DROP promotion_end');
     }
 }

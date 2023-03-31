b1ea2b297e4c2d9bb5485074b0e0c34c3eeac820
Dylan Jacquet <d.jacquet@apparence.io>
2023-03-31 10:54:50 +0200

    add frke

--- src/Form/AddFriendType.php
+++ src/Form/AddFriendType.php
@@ -20,6 +20,9 @@ class AddFriendType extends AbstractType
             ->add('user', EntityType::class, [
                 'class' => User::class,
                 'choice_label' => 'username',
+                'attr' => [
+                    'class' => 'form-control w-full py-1 rounded-lg px-3 focus:outline-none focus:border-electric-purple hover:shadow bg-dark-purple my-4'
+                ],
                 'query_builder' => function (UserRepository $repo) {
                     return $repo->createQueryBuilder('u');
                     // ->where('u.id != :id')
@@ -29,7 +32,7 @@ class AddFriendType extends AbstractType
             ->add('submit', SubmitType::class, [
                 'label' => 'Add friend',
                 'attr' => [
-                    'class' => 'btn btn-primary'
+                    'class' => 'btn btn-primary inline-block rounded-full bg-danger px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#AE4CDC] transition duration-150 ease-in-out hover:bg-danger-600 hover:shadow-[0_8px_9px_-4px_rgba(174,76,220,0.3),0_4px_18px_0_rgba(174,76,220,0.2)] focus:bg-danger-600 focus:shadow-[0_8px_9px_-4px_rgba(174,76,220,0.3),0_4px_18px_0_rgba(174,76,220,0.2)] focus:outline-none focus:ring-0 active:bg-danger-700 active:shadow-[0_8px_9px_-4px_rgba(174,76,220,0.3),0_4px_18px_0_rgba(174,76,220,0.2)]'
                 ]
             ]);
     }
